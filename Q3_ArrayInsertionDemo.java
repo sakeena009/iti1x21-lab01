1ce93f706cffa11a3118b34e0903cd1783614e94
sakeena009 <65218211+sakeena009@users.noreply.github.com>
2020-05-19 14:13:55 -0400

    Update Q3_ArrayInsertionDemo.java

--- Q3_ArrayInsertionDemo.java
+++ Q3_ArrayInsertionDemo.java
@@ -1,10 +1,33 @@
 public class Q3_ArrayInsertionDemo {
 
   public static int[] insertIntoArray(int[] beforeArray, int indexToInsert, int valueToInsert){
-    // Your code here
+    int[] afterArray;
+    afterArray=new int[beforeArray.length+1];
+    for (int i=0; i<indexToInsert; i++){
+      afterArray[i]=beforeArray[i];
+    }
+    afterArray[indexToInsert]=valueToInsert;
+    if (indexToInsert<afterArray.length){
+      for (int i=indexToInsert+1; i<afterArray.length; i++){
+        afterArray[i]=beforeArray[i-1];
+      }
+    }
+    System.out.println("Array after insertion: ");
+    for (int i=0; i<afterArray.length; i++){
+        System.out.println(afterArray[i]);
+    }
+    return afterArray;
   }
 
   public static void main(String[] args){
-    // Your code here
+    int[] array= {1,5,4,7,9,6};
+    System.out.println("Array before insertion:");
+    for (int i=0; i<array.length; i++){
+      System.out.println(array[i]);
+    }
+    int index=3;
+    int value=15;
+    insertIntoArray(array, index, value);
+    
   }
 }
