ccf5a9eeb9b88d002199c6f15f72d42fc30dc9c3
sakeena009 <65218211+sakeena009@users.noreply.github.com>
2020-05-19 14:16:55 -0400

    Update Q3_ReverseSortDemo.java

--- Q3_ReverseSortDemo.java
+++ Q3_ReverseSortDemo.java
@@ -10,8 +10,14 @@ public class Q3_ReverseSortDemo {
 
   //method that sorts a char array into its reverse alphabetical order
   public static void reverseSort(char[] values){
-
-    //your code here
+    for (int i=0; i<values.length; i++){
+        for (int j=i+1; j<values.length; j++){
+            if (values[j]>values[i]){
+                char old=values[i];
+                values[i]=values[j];
+                values[j]=old;
+            }
+        }
+    }
   }
-
 }
